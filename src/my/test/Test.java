a2f35136502484a033065d696d567f6396260fc8
danyang2015 <danyang2008@vip.qq.com>
2015-12-04 15:43:31 +0800

    修改工作编码为UTF-8

--- src/my/test/Test.java
+++ src/my/test/Test.java
@@ -3,7 +3,7 @@ package my.test;
 public class Test {
     
     public static void main(String[] args) {
-        System.out.println("ÕâÊÇÒ»¶Î²âÊÔ´úÂë£¡");
+        System.out.println("è¿™æ˜¯ä¸€æ®µæµ‹è¯•ä»£ç ï¼");
     }
     
 }
