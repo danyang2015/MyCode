fc5a5639659985e2d3f4aa08cfae2c755f98210a
danyang2015 <danyang2008@vip.qq.com>
2015-12-04 15:41:10 +0800

    test

--- src/my/test/Test.java
+++ src/my/test/Test.java
@@ -3,7 +3,7 @@ package my.test;
 public class Test {
     
     public static void main(String[] args) {
-        System.out.println("这是一段测试代码！！");
+        System.out.println("这是一段测试代码！");
     }
     
 }
